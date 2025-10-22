8b828f51dc5c9e60d220088c2d6e1c5c487deef8
Devops-MorelifeLondon <devops@morelifelondon.net>
2025-10-22 15:39:12 +0530

    update

--- middleware/cors.js
+++ middleware/cors.js
@@ -2,7 +2,7 @@
 const cors = require('cors');
 
 const corsOptions = {
-  origin: ['http://localhost:8080', 'http://localhost:8081','https://juris-dash-v2.vercel.app' ],
+  origin: ['http://localhost:8080', 'http://localhost:8081','https://juris-dash-v2.vercel.app', 'https://juris-panel-suite.vercel.app' ],
   credentials: true,
   optionsSuccessStatus: 200
 };
